5a3ab59394b998481e939ec2d09f08f307ce61c8
Anders Jensen Løvig <anders.lloevig@gmail.com>
2022-03-19 00:23:29 +0100

    Added geolocation endpoint

---
+++ Controllers/GeolocationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheMonolith.Models;
+
+namespace TheMonolith.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GeolocationController : ControllerBase
+{
+    public static Location RegnecentralenLocation = new Location
+    {
+        Latitude = 56.1724716m,
+        Longitude = 10.1877707m,
+        Accuracy = 0
+    };
+    
+    [HttpPost]
+    public TestResponse Post([FromBody] Location location)
+    {
+        return new TestResponse
+        {
+            Distance = location.DistanceTo(RegnecentralenLocation)
+        };
+    }
+}
+
+public class TestResponse
+{
+    public decimal Distance { get; set; }
+    public decimal Score { get; set; }
+}
---
+++ Models/Location.cs
@@ -0,0 +1,30 @@
+﻿namespace TheMonolith.Models;
+
+public class Location
+{
+    public decimal Latitude { get; set; }
+    public decimal Longitude { get; set; }
+    public decimal Accuracy { get; set; }
+    
+    public override string ToString() => $"Location: ({Latitude}, {Longitude}) with accuracy {Accuracy}";
+}
+
+public static class LocationExtensions
+{
+    public static decimal DistanceTo(this Location baseCoordinates, Location targetCoordinates)
+    {
+        var baseRad = Math.PI * (double)baseCoordinates.Latitude / 180;
+        var targetRad = Math.PI * (double)targetCoordinates.Latitude/ 180;
+        var theta = baseCoordinates.Longitude - targetCoordinates.Longitude;
+        var thetaRad = Math.PI * (double)theta / 180;
+
+        double dist =
+            Math.Sin(baseRad) * Math.Sin(targetRad) + Math.Cos(baseRad) *
+            Math.Cos(targetRad) * Math.Cos(thetaRad);
+        dist = Math.Acos(dist);
+
+        dist = dist * 180 / Math.PI;
+        dist = dist * 60 * 1.1515;
+        return (decimal)(dist * 1.609344); // Kilometer
+    }
+}
