f84ed47dbcc531dfed0ccbb12838e7ad85c3db79
Kristoffer Strube <kristoffer.strube@gmail.com>
2022-03-19 12:09:32 +0100

    Updated user model

--- Data/User.cs
+++ Data/User.cs
@@ -3,8 +3,21 @@ namespace TheMonolith.Data
     public class User
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
         public string UserName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [MinLength(1)]
         public string Password { get; set; }
     }
 }
