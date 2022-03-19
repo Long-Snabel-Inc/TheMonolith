285fb415693176b15da8485877c4231dd6496319
Kristoffer Strube <kristoffer.strube@gmail.com>
2022-03-19 01:09:43 +0100

    Added CORS

--- Startup.cs
+++ Startup.cs
@@ -14,7 +14,7 @@ using TheMonolith.Database.Repositories;
 public class Startup
 {
     public IConfiguration Configuration { get; }
-    
+
     public Startup(IConfiguration configuration) => Configuration = configuration;
 
     public void ConfigureServices(IServiceCollection services)
@@ -28,26 +28,42 @@ public class Startup
         services.Options<ServiceOptions>(Configuration);
         services.AddPostgres(Configuration);
         services.AddSingleton<ScoreRepository>();
-        
+
+        services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(builder =>
+            {
+                builder
+                .WithOrigins("https://localhost:7180",
+                                "http://localhost:5179",
+                                "https://loevig.net")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+            });
+        });
+
         services.AddRouting();
         services.AddControllers();
-        
+
         services.AddSwaggerGen(ConfigureSwagger);
     }
-    
+
     private void ConfigureSwagger(SwaggerGenOptions config)
     {
-        config.SwaggerDoc("monolith", new OpenApiInfo {Title = "The Monolith API", Version = "v1"});
+        config.SwaggerDoc("monolith", new OpenApiInfo { Title = "The Monolith API", Version = "v1" });
         config.IncludeXmlComments("TheMonolith.xml", true);
         config.IgnoreObsoleteActions();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
     {
-        
+
         app.UseSwagger(options => options.RouteTemplate = "swagger/{documentName}/swagger.json");
         app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/monolith/swagger.json", "The Monolith API"));
-        
+
+        app.UseCors();
+
         app.UseForwardedHeaders();
         app.UseRouting();
         app.UseEndpoints(endpoints =>
