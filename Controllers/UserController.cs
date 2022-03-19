5cac1d4b32d1b4645560aa30c02b10b0774a427b
Kristoffer Strube <kristoffer.strube@gmail.com>
2022-03-19 17:05:17 +0100

    Added Register

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -6,6 +6,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using TheMonolith.Data;
+using TheMonolith.Database.Repositories;
 using TheMonolith.Models;
 
 namespace TheMonolith.Controllers;
@@ -15,43 +16,65 @@ namespace TheMonolith.Controllers;
 public class UserController : ControllerBase
 {
     private readonly ServiceOptions _serviceOptions;
+    private readonly UserRepository _userRepository;
 
-    public UserController(ServiceOptions serviceOptions)
+    public UserController(ServiceOptions serviceOptions, UserRepository userRepository)
     {
         _serviceOptions = serviceOptions;
+        _userRepository = userRepository;
     }
 
     [HttpPost("Authenticate")]
     [AllowAnonymous]
-    public ActionResult Authenticate([FromBody] LoginModel loginModel)
+    public async Task<ActionResult> Authenticate([FromBody] LoginModel loginModel)
     {
-        // Dummy authencation
-        if (loginModel.UserName != "Strube")
-            return BadRequest("You are not Strube");
+        var user = await _userRepository.Get(loginModel.UserName);
+        if (user is null || !await _userRepository.Login(user, loginModel.Password))
+            return BadRequest("Invadlid username/password");
 
-        var user = new User() { UserName = "Strube" };
+        return Ok(new
+        {
+            Id = user.Id,
+            FullName = user.FullName,
+            Email = user.Email,
+            UserName = user.UserName,
+            token = tokenString(user),
+        });
+    }
 
+    [HttpPost("Register")]
+    [AllowAnonymous]
+    public async Task<ActionResult> Register([FromBody] RegisterModel registerModel)
+    {
+        if (await _userRepository.ExistsUsername(registerModel.UserName) || await _userRepository.ExistsEmail(registerModel.Email))
+            return BadRequest("Email or username already excists");
+
+        var user = await _userRepository.Create(registerModel.UserName, registerModel.Email, registerModel.FullName, registerModel.Password);
+
+        return Ok(new
+        {
+            Id = user.Id,
+            FullName = user.FullName,
+            Email = user.Email,
+            UserName = user.UserName,
+            token = tokenString(user),
+        });
+    }
+
+    private string tokenString(User user)
+    {
         var tokenHandler = new JwtSecurityTokenHandler();
         var key = Encoding.ASCII.GetBytes(_serviceOptions.PrivateKey);
         var tokenDescriptor = new SecurityTokenDescriptor
         {
             Subject = new ClaimsIdentity(new Claim[]
             {
-                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                        new Claim(ClaimTypes.Name, user.Id.ToString())
             }),
             Expires = DateTime.UtcNow.AddDays(7),
             SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
         };
         var token = tokenHandler.CreateToken(tokenDescriptor);
-        var tokenString = tokenHandler.WriteToken(token);
-
-        return Ok( new
-        {
-            Id = user.Id,
-            FullName = user.FullName,
-            Email = user.Email,
-            UserName = user.UserName,
-            token = tokenString,
-        });
+        return tokenHandler.WriteToken(token);
     }
 }
