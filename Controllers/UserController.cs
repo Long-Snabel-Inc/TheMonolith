3b40959236a0b6a1f06374f72cb8097f679546ba
Kristoffer Strube <kristoffer.strube@gmail.com>
2022-03-19 12:03:57 +0100

    Started work on User Authentication

---
+++ Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TheMonolith.Data;
+
+namespace TheMonolith.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly ServiceOptions serviceOptions;
+
+    public UserController(ServiceOptions serviceOptions)
+    {
+        this.serviceOptions = serviceOptions;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns></returns>
+    [HttpPost("Authenticate")]
+    public IActionResult Authenticate(User user)
+    {
+        // Dummy authencation
+        if (user.UserName != "Strube")
+            return BadRequest("You are not Strube");
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(serviceOptions.PrivateKey);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+            }),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var tokenString = tokenHandler.WriteToken(token);
+
+        return Ok( tokenString );
+    }
+}
--- Data/User.cs
+++ Data/User.cs
@@ -1,8 +1,10 @@
 namespace TheMonolith.Data
 {
-    public record User(string UserName, string Email)
+    public class User
     {
         public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
     }
 }
