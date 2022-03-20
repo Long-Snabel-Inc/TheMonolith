e5e651a44208a072caf8dee353424c1f485eb95e
Anders Jensen Løvig <anders.lloevig@gmail.com>
2022-03-20 01:10:16 +0100

    GetClosestUser

--- Data/Score.cs
+++ Data/Score.cs
@@ -4,6 +4,7 @@ namespace TheMonolith.Data
     {
         public const string GoogleType = "GOOGLETEXT";
         public const string LocationType = "LOCATION";
+        public const string PeopleLocationType = "PEOPLE_LOCATION";
         
         public int Id { get; set; }
     }
